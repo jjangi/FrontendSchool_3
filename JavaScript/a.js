fbcf722e18a4d89345ab65d5c56009395138c9a3
isol.bae <65801741+jjangi@users.noreply.github.com>
2022-10-06 17:05:15 +0900

    Add files via upload

---
+++ JavaScript/a.js
@@ -0,0 +1,7 @@
+//var 변수 = 30;
+//let 변수 = 10;
+const 변수 = 10;
+
+function 함수(a, b) {
+  return a + b;
+}
---
+++ JavaScript/b.js
@@ -0,0 +1,7 @@
+//var 변수 = 50;
+//let 변수 = 20;
+const 변수 = 10;
+
+function 함수(a, b) {
+  return a * b;
+}
