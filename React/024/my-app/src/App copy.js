15561fd5a080b6edb7bd2acb9390804e4672b06b
isol.bae <65801741+jjangi@users.noreply.github.com>
2022-11-14 16:47:13 +0900

    1114_React

---
+++ React/024/my-app/src/App copy.js
@@ -0,0 +1,28 @@
+function Resume({ 이름, 취미, 자기소개 }) {
+  let like = 0;
+
+  function clickLike() {
+    like += 1;
+    console.log(like);
+  }
+
+  return (
+    <section>
+      <h2>{이름}</h2>
+      <p>{취미}</p>
+      <p>{자기소개}</p>
+      {/* 여기가 왜 증가되지 않을까요? -> 변수가 변경이 된다고 하더라도 렌더링이 다시 일어나지 않기 때문입니다! */}
+      <button onClick={clickLike}>{like}</button>
+    </section>
+  );
+}
+
+function App() {
+  return (
+    <main>
+      <Resume 이름="배이솔" 취미="코딩" 자기소개="안녕하슈!" />
+    </main>
+  );
+}
+
+export default App;
